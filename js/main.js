113af9becf7cc77fe9bc92500714b02c3dbb0470
Kevin Gaudin <kevin.gaudin@gmail.com>
2014-09-06 18:46:46 +0200

    Cast API initialization

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var applicationID = "6F1E40A5";
+
 window['__onGCastApiAvailable'] = function(loaded, errorInfo) {
   if (loaded) {
     console.log("Cast API available.");
@@ -5,4 +7,28 @@ window['__onGCastApiAvailable'] = function(loaded, errorInfo) {
   } else {
     console.log(errorInfo);
   }
+}
+
+initializeCastApi = function() {
+
+  var sessionRequest = new chrome.cast.SessionRequest(applicationID);
+  var apiConfig = new chrome.cast.ApiConfig(sessionRequest, sessionListener,
+    receiverListener);
+  chrome.cast.initialize(apiConfig, onInitSuccess, onError);
+};
+
+sessionListener = function() {
+  console.log("Session listener");
+}
+
+receiverListener = function() {
+  console.log("Receiver listener");
+}
+
+onInitSuccess = function() {
+  console.log("Cast API intialized.");
+}
+
+onError = function() {
+  console.log("Cast API int error.");
 }
