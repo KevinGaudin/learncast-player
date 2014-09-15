44004fcd445a4f9d1244e3081d63bade9adea071
Kevin Gaudin <kevin.gaudin@gmail.com>
2014-09-15 21:44:04 +0200

    A few comments don't hurt

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var applicationID = "6F1E40A5";
 var appSession = null;
-// TOO
 
+// Callback called when Cast extension is available
 window['__onGCastApiAvailable'] = function(loaded, errorInfo) {
   if (loaded) {
     console.log("Cast API available.");
@@ -11,19 +11,26 @@ window['__onGCastApiAvailable'] = function(loaded, errorInfo) {
   }
 }
 
+/**
+ * Cast extension is available, init contact.
+ */
 initializeCastApi = function() {
-
   var sessionRequest = new chrome.cast.SessionRequest(applicationID);
   var apiConfig = new chrome.cast.ApiConfig(sessionRequest, sessionListener,
     receiverListener);
-  chrome.cast.initialize(apiConfig, onInitSuccess, onError);
+  chrome.cast.initialize(apiConfig, onInitSuccess, onInitError);
 };
 
-sessionListener = function(e) {
-  console.log("Session listener");
-  console.log(e);
+/* API Init OK */
+onInitSuccess = function() {
+  console.log("Cast API intialized.");
 }
 
+onInitError = function() {
+  console.log("Cast API init error.");
+}
+
+/* Receiver events */
 receiverListener = function(e) {
   console.log("Receiver listener");
   if (e === chrome.cast.ReceiverAvailability.AVAILABLE) {
@@ -31,19 +38,20 @@ receiverListener = function(e) {
   }
 }
 
-onInitSuccess = function() {
-  console.log("Cast API intialized.");
+/* Session events */
+sessionListener = function(e) {
+  console.log("Session listener");
+  console.log(e);
 }
 
-onError = function() {
-  console.log("Cast API init error.");
-}
-
+/* Request app session */
 startAppSession = function() {
   console.log("Request app session")
-  chrome.cast.requestSession(onRequestSessionSuccess, onLaunchError);
+  chrome.cast.requestSession(onRequestSessionSuccess, onRequestSessionError);
 }
 
+
+/* Session started */
 onRequestSessionSuccess = function(e) {
   console.log("App session request success");
   console.log(e);
@@ -52,26 +60,33 @@ onRequestSessionSuccess = function(e) {
   appSession.addUpdateListener(sessionUpdated);
 }
 
-onLaunchError = function(e) {
+/* Error when starting session */
+onRequestSessionError = function(e) {
   console.log("App session request error: ");
   console.log(e);
 }
 
+
+/* Session events */
 sessionUpdated = function(e) {
   console.log("Session update received");
   console.log(e);
 }
 
+
+/* Request session end */
 stopApp = function() {
   console.log("Stop app session.")
   appSession.stop(onStopSuccess, onStopError);
 }
 
+/* Error while requesting session end */
 onStopError = function(e) {
   console.log("App stop error");
   console.log(e);
 }
 
+/* Session ended. */
 onStopSuccess = function(e) {
   console.log("App stopped.");
 }
